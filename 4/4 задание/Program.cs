3265cb123f72f8be4250fec60692ed9f77abab2f
saurondi <90340966+saurondi@users.noreply.github.com>
2022-06-14 23:49:13 +0700

    Add files via upload

---
+++ 4/4 задание/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Привет, до какого числа будем гадать?");
+int n = int.Parse(Console.ReadLine());
+int k;
+int c = 0;
+Random rand = new Random();
+int num = rand.Next(1,n);
+int error = rand.Next(5, 10);
+int error2 = error;
+Console.WriteLine($"У вас есть {error} попыток!");
+Console.WriteLine("Пробуйте угадать!");
+for (int i = 0; i < error2; i++)
+{
+    k = int.Parse(Console.ReadLine());
+    if (k < num)
+    {
+        Console.WriteLine("Число меньше загаданного!");
+        error = error - 1;
+        Console.WriteLine($"Пробуйте дальше у вас осталось: {error} попыток!");
+    }
+    if (k > num)
+    {
+        Console.WriteLine("Число больше загаданного!");
+        error = error - 1;
+        Console.WriteLine($"Пробуйте дальше у вас осталось: {error} попыток!");
+    }
+    if (k == num)
+    {
+        Console.WriteLine($"Вы угадали число - {num}!");
+        break;
+    }
+    c++;
+    if (c == error2)
+    {
+        Console.WriteLine($"Увы у вас не получилось угадать число. Им было - {num} :((((");
+        break;
+    }
+}
