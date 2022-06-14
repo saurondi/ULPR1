bab84531d5262da30d21ed806449a41beb8d3bfb
saurondi <90340966+saurondi@users.noreply.github.com>
2022-06-14 23:57:09 +0700

    Add files via upload

---
+++ Jobs from partner1/№3/№3/Program.cs
@@ -0,0 +1,13 @@
+﻿int n;
+int[] arr = new int[10];
+for (int i = 0; i < arr.Length; i++)
+{
+    n = new Random().Next(1, 100);
+    arr[i] = n;
+    Console.WriteLine(arr[i]);
+}
+var even = arr.Where((element, index)=>index%2 !=0);
+Console.WriteLine();
+Console.WriteLine(even.Count());
+Console.WriteLine();
+Console.WriteLine(even.Sum());
