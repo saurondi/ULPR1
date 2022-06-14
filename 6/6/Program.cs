5f45938547f9e8bfdcfe554906b08fed652bb5d2
saurondi <90340966+saurondi@users.noreply.github.com>
2022-06-14 23:49:51 +0700

    Add files via upload

---
+++ 6/6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите площадь: ");
+int n = int.Parse(Console.ReadLine());
+for (int x = 1; x <= 5; x++)
+{
+    int a = x * x;
+    for (int y = 1; y <= 5; y++)
+    {
+        int b = y * y;
+        for (int z = 1; z <= 5; z++)
+        {
+            int c = z * z;
+            int sum = a + b;
+            double S = (x * y) / 2;
+            if (sum == c)
+            {
+                if (n > S)
+                {
+                    Console.WriteLine($"Стороны треугольника a={x} b={y} c={z} ");
+                    Console.WriteLine($"Площадь треугольника S={S} ");
+                }
+            }
+        }
+    }
+}
