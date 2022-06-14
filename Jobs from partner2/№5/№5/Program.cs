bafd46bd3f9414081425db37456dc357300aa51e
saurondi <90340966+saurondi@users.noreply.github.com>
2022-06-14 23:58:28 +0700

    Add files via upload

---
+++ Jobs from partner2/№5/№5/Program.cs
@@ -0,0 +1,11 @@
+﻿int r;
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    r = i * i;
+    if (r > n)
+    {
+        Console.WriteLine($"Первое число, квадрат которого больше n: {i}");
+        break;
+    }
+}
